171f2009c14c04ad3b23397ef7c0255801db8071
Kitae's Github <aqua0405@gmail.com>
2024-04-18 00:57:11 +0900

    Update 02_layout.js

--- src/02_layout.js
+++ src/02_layout.js
@@ -2,32 +2,40 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal" />
-        <Button title="Add Goal" />
+    <View style={{
+      padding: 50,                      // 여백 50
+      flexDirection: "row",             // 행으로 정렬
+      width: "80%",                     // 너비 80%
+      height: 300,                      // 높이 300
+      justifyContent: "space-around",   // 
+      alignItems: "stretch",            // 
+    }}>
+      <View style={{
+        backgroundColor: "red",         // 배경색 빨간색
+        flex: 1,                        // 비율 1
+        justifyContent: "center",       // 가로 중앙 정렬
+        alignItems: "center",           // 세로 중앙 정렬
+      }}>        
+        <Text>1</Text>
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={{
+        backgroundColor: "blue",        // 배경색 파란색
+        flex: 1,                        // 비율 1
+        justifyContent: "center",       // 가로 중앙 정렬
+        alignItems: "center",           // 세로 중앙 정렬
+      }}>
+        <Text>2</Text>
+      </View>
+      <View style={{
+        backgroundColor: "green",       // 배경색 녹색
+        flex: 1,                        // 비율 1
+        justifyContent: "center",       // 가로 중앙 정렬
+        alignItems: "center",           // 세로 중앙 정렬
+      }}>
+        <Text>3</Text>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  appContainer: {
-    padding: 50,                        // 패딩 50 
-  },
-  inputContainer: {
-    flexDirection: "row",               // 행으로 정렬      
-    justifyContent: "space-between",    // 두 요소 사이의 공간을 최대한 확보
-    alignItems: "center",               // 세로 중앙 정렬
-  },
-  textInput: {
-    borderColor: "#cccccc",     // 테두리 색상
-    borderWidth: 1,             // 테두리 두께
-    padding: 8,                // 패딩 10
-    width: "80%",               // 너비 80%
-    marginRight: 8,             // 오른쪽 마진 8
-  },
-});
+
