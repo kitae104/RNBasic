1398e2159153e31afba7f990ed23b22dc817dabb
Kitae's Github <aqua0405@gmail.com>
2024-04-23 00:32:18 +0900

    Update App.js

--- App.js
+++ App.js
@@ -27,8 +27,12 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal, index) => (     // courseGoals 배열을 순회하며 각 요소를 출력
-          <Text key={index}>{goal}</Text>       // 각 요소를 출력하되, key 속성은 index로 설정
+        {courseGoals.map((goal) => (
+          <View style={styles.goalItem} key={goal}>
+            <Text style={styles.goalText}>
+              {goal}
+            </Text>
+          </View>
         ))}
       </View>
     </View>
@@ -50,7 +54,6 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1, // 아래쪽 테두리 두께 1
     borderBottomColor: "#cccccc", // 아래쪽 테두리 색상
   },
-
   textInput: {
     borderColor: "#cccccc", // 테두리 색상
     borderWidth: 1, // 테두리 두께
@@ -60,5 +63,15 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: 'white',
+  },
+  goalText: {
+    color: 'white',
   },
 });
