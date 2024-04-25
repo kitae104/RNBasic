ed3be65eac04989347a81c53c11a85c7ca39a386
Kitae's Github <aqua0405@gmail.com>
2024-04-25 16:07:43 +0900

    240425-1607

--- App.js
+++ App.js
@@ -1,49 +1,72 @@
 import { useState } from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false); // 모달 상태 설정추가
+
   const [courseGoals, setCourseGoals] = useState([]);
+
+  function startAddGoalHandler() {
+    setModalIsVisible(true); // 모달이 보이도록 설정
+  }
+
+  function endAddGoalHandler() {
+    setModalIsVisible(false); // 모달이 보이지 않도록 설정
+  }
 
   function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
       { text: enteredGoalText, id: Math.random().toString() },
     ]);
+    endAddGoalHandler(); // 모달 닫기
   }
 
   function deleteGoalHandler(id) {
-    setCourseGoals(currentCourseGoals => {
-      return currentCourseGoals.filter((goal) => goal.id !== id); // id가 같지 않은 것만 필터링 ==> 삭제 
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id); // id가 같지 않은 것만 필터링 ==> 삭제
     });
   }
 
   return (
-    <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler} />
-
-      <View style={styles.goalsContainer}>
-        <FlatList /* 목록이 길어지는 경우 ScrollView 대신 FlatList 사용 */
-          data={courseGoals}
-          renderItem={(itemData) => {
-            return (
-              <GoalItem
-                text={itemData.item.text}
-                id={itemData.item.id}
-                onDeleteItem={deleteGoalHandler}
-              /> /* text를 이용하여 정보 전달 */
-            );
-          }}
-          keyExtractor={(item, index) => {
-            /* keyExtractor: 각 아이템의 고유한 키를 지정 */
-            return item.id;
-          }}
-          alwaysBounceVertical={false} /* 수직 스크롤의 바운스 여부 */
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <Button
+          title="Add New Goal"
+          color="#5e0acc"
+          onPress={startAddGoalHandler}
         />
+        <GoalInput
+          visible={modalIsVisible}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+        <View style={styles.goalsContainer}>
+          <FlatList /* 목록이 길어지는 경우 ScrollView 대신 FlatList 사용 */
+            data={courseGoals}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteGoalHandler}
+                /> /* text를 이용하여 정보 전달 */
+              );
+            }}
+            keyExtractor={(item, index) => {
+              /* keyExtractor: 각 아이템의 고유한 키를 지정 */
+              return item.id;
+            }}
+            alwaysBounceVertical={false} /* 수직 스크롤의 바운스 여부 */
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -52,6 +75,7 @@ const styles = StyleSheet.create({
     flex: 1, // 전체 화면 사용
     padding: 50, // 패딩 50
     paddingHorizontal: 16, // 가로 패딩 16
+    backgroundColor: "#1e085a",
   },
 
   goalsContainer: {
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,9 +1,8 @@
-import { View, TextInput, Button } from "react-native";
+import { View, TextInput, Button, Modal, Image } from "react-native";
 import { StyleSheet } from "react-native";
 import { useState } from "react";
 
 function GoalInput(props) {
-
   const [enteredGoalText, setEnteredGoalText] = useState("");
 
   function goalInputHandler(enteredText) {
@@ -12,19 +11,32 @@ function GoalInput(props) {
 
   function addGoalHandler() {
     props.onAddGoal(enteredGoalText); // 입력한 텍스트를 App.js로 전달
-    setEnteredGoalText("");           // 입력창 초기화
+    setEnteredGoalText(""); // 입력창 초기화
   }
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput /* TextInput: 사용자로부터 텍스트를 입력받을 수 있는 입력 컴포넌트 */
-        style={styles.textInput}
-        placeholder="Your course goal"
-        onChangeText={goalInputHandler}   // 입력한 텍스트를 goalInputHandler로 전달
-        value={enteredGoalText}           // 입력한 텍스트를 value로 설정 - 초기화 
-      />
-      <Button title="Add Goal" onPress={addGoalHandler} />
-    </View>
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
+        <TextInput /* TextInput: 사용자로부터 텍스트를 입력받을 수 있는 입력 컴포넌트 */
+          style={styles.textInput}
+          placeholder="Your course goal"
+          onChangeText={goalInputHandler} // 입력한 텍스트를 goalInputHandler로 전달
+          value={enteredGoalText} // 입력한 텍스트를 value로 설정 - 초기화
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} color="#5e0acc" />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 }
 
@@ -33,18 +45,35 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: "row", // 행으로 정렬
-    justifyContent: "space-between", // 두 요소 사이의 공간을 최대한 확보
+    //flexDirection: "center", // 행으로 정렬
+    justifyContent: "center", // 두 요소 사이의 공간을 최대한 확보
     alignItems: "center", // 세로 중앙 정렬
-    marginBottom: 24, // 아래쪽 패딩 24
+    //marginBottom: 24, // 아래쪽 패딩 24
+    padding: 16, // 전체 패딩 16
     borderBottomWidth: 1, // 아래쪽 테두리 두께 1
     borderBottomColor: "#cccccc", // 아래쪽 테두리 색상
+    backgroundColor: "#311b6b",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
   textInput: {
-    borderColor: "#cccccc", // 테두리 색상
     borderWidth: 1, // 테두리 두께
-    padding: 8, // 패딩 10
-    width: "70%", // 너비 70%
-    marginRight: 8, // 오른쪽 마진 8
+    borderColor: "#e4d0ff", // 테두리 색상
+    backgroundColor: '#e4d0ff',
+    color: '#120438',
+    borderRadius: 6, // 테두리 둥글기 6
+    width: "100%", // 너비 70%
+    padding: 16, // 패딩 16
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row", // 행으로 정렬
+  },
+  button: {
+    width: "30%", // 너비 40%
+    marginHorizontal: 8, // 좌우 마진 8
   },
 });
